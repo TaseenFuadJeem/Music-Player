5abd52e7530fc0992ddd4cec2d4417243306d6b4
Md. Ta-Seen Fuad Jeem <mdtaseenfuad@gmail.com>
2022-06-28 20:19:59 +0600

    Added the songs into an array

--- src/Components/Playlist/AllPlaylist.js
+++ src/Components/Playlist/AllPlaylist.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import { CgPlayList } from 'react-icons/cg';
 
 const AllPlaylist = ({ allPlaylist }) => {
 
     return (
-        <button className='btn relative mx-3 bg-white text-black hover:text-white hover:bg-gray-500'>{allPlaylist} <span className='ml-5'>🎶</span></button>
+        <button className='btn relative mx-3 bg-white text-black hover:text-white hover:bg-gray-500'>{allPlaylist} <CgPlayList className='text-2xl ml-3 relative top-[2px]' /></button>
     );
 };
 
--- src/Components/Playlist/Songs.js
+++ src/Components/Playlist/Songs.js
@@ -1,24 +1,27 @@
 import React from 'react';
 import { ImMusic } from 'react-icons/im';
+import { RiPlayListAddFill } from 'react-icons/ri';
 
 const Songs = () => {
+
+    const allSongs = [
+        'The Weeknd - Starboy ft.Daft Punk(Official Audio)',
+        'The Weeknd - Starboy ft.Daft Punk(Official Audio)',
+        'The Weeknd - Starboy ft.Daft Punk(Official Audio)',
+        'The Weeknd - Starboy ft.Daft Punk(Official Audio)',
+        'The Weeknd - Starboy ft.Daft Punk(Official Audio)',
+        'The Weeknd - Starboy ft.Daft Punk(Official Audio)',
+    ]
+
     return (
         <section className='mb-6'>
-            <div className='bg-white rounded-lg p-5 flex items-center'>
-                <ImMusic className='mr-3 text-lg' /> <p className='font-semibold'>The Weeknd - Starboy ft. Daft Punk (Official Audio)</p>
-            </div>
-            <div className='bg-white rounded-lg p-5 flex items-center mt-2'>
-                <ImMusic className='mr-3 text-lg' /> <p className='font-semibold'>The Weeknd - Starboy ft. Daft Punk (Official Audio)</p>
-            </div>
-            <div className='bg-white rounded-lg p-5 flex items-center mt-2'>
-                <ImMusic className='mr-3 text-lg' /> <p className='font-semibold'>The Weeknd - Starboy ft. Daft Punk (Official Audio)</p>
-            </div>
-            <div className='bg-white rounded-lg p-5 flex items-center mt-2'>
-                <ImMusic className='mr-3 text-lg' /> <p className='font-semibold'>The Weeknd - Starboy ft. Daft Punk (Official Audio)</p>
-            </div>
-            <div className='bg-white rounded-lg p-5 flex items-center mt-2'>
-                <ImMusic className='mr-3 text-lg' /> <p className='font-semibold'>The Weeknd - Starboy ft. Daft Punk (Official Audio)</p>
-            </div>
+            {
+                allSongs.map(song =>
+                    <div className='bg-white rounded-lg p-5 flex items-center mb-3 relative'>
+                        <ImMusic className='mr-3 text-lg' /> <p className='font-semibold'>{song}</p> <RiPlayListAddFill className='ml-auto text-xl cursor-pointer' />
+                    </div>
+                )
+            }
         </section>
     );
 };
